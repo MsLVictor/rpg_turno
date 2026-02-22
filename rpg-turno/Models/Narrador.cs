baa81945682c0726cc70123059d0151138f62e27
MsLVictor <victormleite25@icloud.com>
2026-02-22 15:43:35 -0300

    feat/victor-leite: Cria Narrador.NarrandoDevagar();

--- rpg-turno/Models/Narrador.cs
+++ rpg-turno/Models/Narrador.cs
@@ -41,4 +41,22 @@ public static class Narrador
         Thread.Sleep(400);
         Console.Clear();
     }
+
+    public static void CriandoPersonagem()
+    {
+        NarrandoDevagar("Você acorda em um descampado em um local desconhecido para você...");
+        Console.Read();
+        NarrandoDevagar("Você não lembra de caralho nenhum, O que PORRA aconteceu? - Você pensou...");
+
+    }
+
+    public static void NarrandoDevagar(string texto)
+    {
+        foreach (char letra in texto)
+        {
+            Console.Write(letra);
+            Thread.Sleep(30);
+        }
+        Console.WriteLine("\n");
+    }
 }
